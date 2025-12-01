f19297a7d13b885437a7f3cdbdd77d0cf595c449
Ayrat Safin <airex.c@gmail.com>
2025-12-01 23:51:13 +0500

    day 1 part 2

--- day_1.py
+++ day_1.py
@@ -5,23 +5,33 @@ count_of_zeros = 0
 init_value = 50
 MIN_VALUE = 0
 MAX_VALUE = 99
+COUNT = MAX_VALUE + 1
 
 
 def get_dial_position(position: int, rotation: str):
     direction = rotation[0]
     shift = int(rotation[1:]) % 100
-    dial = list(range(MIN_VALUE, MAX_VALUE + 1))
+    zero_passes = int(rotation[1:]) // 100
+    dial = list(range(MIN_VALUE, COUNT))
 
     current_position = position - shift if direction == "L" else position + shift
 
     if current_position < MIN_VALUE:
-        position = dial[current_position]
+        new_position = dial[current_position]
+        zero_passes = (
+            zero_passes + 1 if new_position != 0 and position != 0 else zero_passes
+        )
     elif current_position > MAX_VALUE:
-        position = dial[current_position - 100]
+        new_position = dial[current_position - COUNT]
+        zero_passes = zero_passes + 1 if new_position != 0 else zero_passes
     else:
-        position = dial[current_position]
+        new_position = dial[current_position]
 
-    return dial[position]
+    print(
+        f"{position} {'+ ' if direction == 'R' else '-'} {rotation} -> {new_position} {zero_passes}"
+    )
+
+    return [dial[new_position], zero_passes]
 
 
 position = 50
@@ -38,9 +48,12 @@ if len(sys.argv) == 2:
 
 
 for item in rotations:
-    position = get_dial_position(position, item)
+    [position, zero_passes] = get_dial_position(position, item)
 
     if position == 0:
         count_of_zeros += 1
+
+    # only for part 2
+    count_of_zeros += zero_passes
 
 print(count_of_zeros)
